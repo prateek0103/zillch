a1e83fb00d9fab486d1ea6469248294f3c3d7e23
Prateek Gupta <aryan.838@gmail.com>
2016-08-19 18:12:05 +0530

    First commit
    Lets use github, for all our future commits, this way we can track the
    builds.

---
+++ script.js
@@ -0,0 +1,157 @@
+/**
+ * Create the module. Set it up to use html5 mode.
+ */
+window.MyOpenRecipes = angular.module('myOpenRecipes', ['elasticsearch', 'ngAnimate', 'ngRoute'],
+    ['$locationProvider', function($locationProvider){
+        $locationProvider.html5Mode({enabled : true, requireBase: false});
+    }]
+);
+
+/**
+ * Create a service to power calls to Elasticsearch. We only need to
+ * use the _search endpoint.
+ */
+MyOpenRecipes.factory('recipeService',
+    ['$q', 'esFactory', '$location', function($q, elasticsearch, $location){
+        var client = elasticsearch({
+            host: "http://admin:elasticsearch@node2016@5483a9ea4e388ef915897a5f32c43023.ap-southeast-1.aws.found.io:9200"
+
+        });
+
+        /**
+         * Given a term and an offset, load another round of 10 recipes.
+         *
+         * Returns a promise.
+         */
+        var search = function(term, offset){
+
+            var deferred = $q.defer();
+            var query = {
+                "match": {
+                    "_all": term
+                }
+            };
+
+            client.search({
+                "index": 'recipes',
+                "type": 'recipe',
+                "body": {
+                    "size": 10,
+                    "from": (offset || 0) * 10,
+                    "query": query
+                }
+            }).then(function(result) {
+                var ii = 0, hits_in, hits_out = [];
+                hits_in = (result.hits || {}).hits || [];
+                for(;ii < hits_in.length; ii++){
+                    hits_out.push(hits_in[ii]._source);
+                }
+                deferred.resolve(hits_out);
+            }, deferred.reject);
+
+            return deferred.promise;
+        };
+
+
+        return {
+            "search": search
+        };
+    }]
+);
+
+/**
+ * Create a controller to interact with the UI.
+ */
+MyOpenRecipes.controller('recipeCtrl',
+    ['recipeService', '$scope', '$location', function(recipes, $scope, $location){
+
+
+        // Initialize the scope defaults.
+        $scope.recipes = [];        // An array of recipe results to display
+        $scope.page = 0;            // A counter to keep track of our current page
+        $scope.allResults = false;  // Whether or not all results have been found.
+        $scope.my = { isSearched: false };
+
+        // And, a random search term to start if none was present on page load.
+       // $scope.searchTerm = $location.search().q;
+
+        /**
+         * A fresh search. Reset the scope variables to their defaults, set
+         * the q query parameter, and load more results.
+         */
+        $scope.checkEnter = function($event){
+            var key = $event.which || $event.key;
+            //console.log(key);
+            if(key === 13)
+                $scope.search();
+        }
+
+        $scope.doLogin = function(){
+            console.log("works");
+        }
+        $scope.noResult = function(){
+            if($scope.my.isSearched){
+                if($scope.recipes.length>0)
+                    return true;
+                else return false;
+            }
+            else return true;
+        }
+
+
+        $scope.search = function(){
+
+
+
+
+                $scope.my.isSearched = true;
+                $scope.recipes.length=0;
+
+
+           // console.log($scope.recipes.length);
+            var elementOnce = angular.element(document.querySelector('#once'));
+            elementOnce.addClass('ng-hide');
+            $scope.page = 0;
+            $scope.recipes = [];
+            $scope.allResults = false;
+            console.log($scope.searchTerm);
+            $location.search({'q': $scope.searchTerm});
+           // $scope.loadMore();
+        };
+
+        /**
+         * Load the next page of results, incrementing the page counter.
+         * When query is finished, push results onto $scope.recipes and decide
+         * whether all results have been returned (i.e. were 10 results returned?)
+         */
+        $scope.loadMore = function(){
+            recipes.search($scope.searchTerm, $scope.page++).then(function(results){
+                if(results.length !== 10){
+                    $scope.allResults = true;
+                }
+
+                var ii = 0;
+                for(;ii < results.length; ii++){
+                    $scope.recipes.push(results[ii]);
+                }
+            });
+        };
+
+        // Load results on first run
+      //  $scope.loadMore();
+    }]
+);
+
+MyOpenRecipes.config(function($routeProvider){
+    $routeProvider
+    .when("/", {
+        templateUrl : "root.html",
+        controller : 'recipeCtrl'
+    })
+    .when("/login", {
+        templateUrl : "login.html"
+    })
+    .when("/signup", {
+        templateUrl : "signup.html"
+    });
+});
